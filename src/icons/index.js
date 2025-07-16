5b05b2e593bdff01354bd102184e2a342f96dcf3
Aleksei-Uzin <aleksei.uzin@gmail.com>
2025-07-16 20:05:36 +0300

    refactor: add import of SVG icons as React components

---
+++ src/icons/index.js
@@ -0,0 +1,41 @@
+import ArrowDown from './svgs/arrow-down.svg?react'
+import ArrowLeft from './svgs/arrow-left.svg?react'
+import ArrowRight from './svgs/arrow-right.svg?react'
+import Bell from './svgs/bell.svg?react'
+import Box from './svgs/box.svg?react'
+import Camera from './svgs/camera.svg?react'
+import CandyBox from './svgs/candy-box.svg?react'
+import Clock from './svgs/clock.svg?react'
+import Documents from './svgs/documents.svg?react'
+import Game from './svgs/game.svg?react'
+import Gear from './svgs/gear.svg?react'
+import Heart from './svgs/heart.svg?react'
+import History from './svgs/history.svg?react'
+import Home from './svgs/home.svg?react'
+import MenuKebab from './svgs/menu-kebab.svg?react'
+import Menu from './svgs/menu.svg?react'
+import Music from './svgs/music.svg?react'
+import Search from './svgs/search.svg?react'
+import Star from './svgs/star.svg?react'
+import Torch from './svgs/torch.svg?react'
+
+export const IconArrowDown = ArrowDown
+export const IconArrowLeft = ArrowLeft
+export const IconArrowRight = ArrowRight
+export const IconBell = Bell
+export const IconBox = Box
+export const IconCamera = Camera
+export const IconCandyBox = CandyBox
+export const IconClock = Clock
+export const IconDocuments = Documents
+export const IconGame = Game
+export const IconGear = Gear
+export const IconHeart = Heart
+export const IconHistory = History
+export const IconHome = Home
+export const IconMenuKebab = MenuKebab
+export const IconMenu = Menu
+export const IconMusic = Music
+export const IconSearch = Search
+export const IconStar = Star
+export const IconTorch = Torch
