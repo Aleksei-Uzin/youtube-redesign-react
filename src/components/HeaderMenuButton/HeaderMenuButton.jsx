2912a555f15cdb453ba3a516ce76d377b9e4dc25
Aleksei-Uzin <aleksei.uzin@gmail.com>
2025-07-18 13:58:14 +0300

    refactor: HeaderMenuButton

--- src/components/HeaderMenuButton/HeaderMenuButton.jsx
+++ src/components/HeaderMenuButton/HeaderMenuButton.jsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from 'react'
 import { Button } from '../Button'
 import { IconMenu } from '../../icons'
+import { useHeaderMenuButton } from './useHeaderMenuButton'
 import { useMedia } from '../../hooks/useMedia'
 import styles from './headerMenuButton.module.css'
 
 export const HeaderMenuButton = () => {
+  const [, setButtonState] = useHeaderMenuButton()
   const isMatchMedia = useMedia('(max-width: 660px)')
-  const [state, setState] = useState(isMatchMedia)
+  const [media, setMedia] = useState(isMatchMedia)
 
-  useEffect(() => void setState(isMatchMedia), [isMatchMedia])
+  useEffect(() => void setMedia(isMatchMedia), [isMatchMedia])
+
+  const handleClick = () => setButtonState(prev => !prev)
 
   return (
-    <Button className={styles[state ? 'header-menu-button_none' : '']}>
+    <Button
+      className={styles[media ? 'header-menu-button_none' : '']}
+      onClick={handleClick}
+    >
       <IconMenu />
     </Button>
   )
