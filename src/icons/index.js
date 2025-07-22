56b730bd94edcec27189ca04aa51da6d62607aac
Aleksei-Uzin <aleksei.uzin@gmail.com>
2025-07-22 12:33:58 +0300

    feat: add new icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -6,18 +6,29 @@ import Box from './svgs/box.svg?react'
 import Camera from './svgs/camera.svg?react'
 import CandyBox from './svgs/candy-box.svg?react'
 import Clock from './svgs/clock.svg?react'
+import Dislike from './svgs/dislike.svg?react'
 import Documents from './svgs/documents.svg?react'
+import FullScreen from './svgs/full-screen.svg?react'
 import Game from './svgs/game.svg?react'
 import Gear from './svgs/gear.svg?react'
 import Heart from './svgs/heart.svg?react'
 import History from './svgs/history.svg?react'
 import Home from './svgs/home.svg?react'
+import Like from './svgs/like.svg?react'
+import MenuKebabHorizontal from './svgs/menu-kebab-horizontal.svg?react'
 import MenuKebab from './svgs/menu-kebab.svg?react'
 import Menu from './svgs/menu.svg?react'
 import Music from './svgs/music.svg?react'
+import Next from './svgs/next.svg?react'
+import Pause from './svgs/pause.svg?react'
 import Search from './svgs/search.svg?react'
+import Share from './svgs/share.svg?react'
+import Size from './svgs/size.svg?react'
 import Star from './svgs/star.svg?react'
+import Subtitles from './svgs/subtitles.svg?react'
 import Torch from './svgs/torch.svg?react'
+import Volume from './svgs/volume.svg?react'
+import VideoPlayerGear from './svgs/videoplayer-gear.svg?react'
 import YouTubeLogo from './svgs/youtube-logo.svg?react'
 
 export const IconArrowDown = ArrowDown
@@ -28,16 +39,27 @@ export const IconBox = Box
 export const IconCamera = Camera
 export const IconCandyBox = CandyBox
 export const IconClock = Clock
+export const IconDislike = Dislike
 export const IconDocuments = Documents
+export const IconFullScreen = FullScreen
 export const IconGame = Game
 export const IconGear = Gear
 export const IconHeart = Heart
 export const IconHistory = History
 export const IconHome = Home
+export const IconLike = Like
+export const IconMenuKebabHorizontal = MenuKebabHorizontal
 export const IconMenuKebab = MenuKebab
 export const IconMenu = Menu
 export const IconMusic = Music
+export const IconNext = Next
+export const IconPause = Pause
 export const IconSearch = Search
+export const IconShare = Share
+export const IconSize = Size
 export const IconStar = Star
+export const IconSubtitles = Subtitles
 export const IconTorch = Torch
+export const IconVolume = Volume
+export const IconVideoPlayerGear = VideoPlayerGear
 export const IconYouTubeLogo = YouTubeLogo
